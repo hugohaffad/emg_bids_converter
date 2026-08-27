2269f8bc4eaa5e1fb994a1ba47fd60db55e440c7
Hugo Haffad <hugohaffad@icloud.com>
2026-08-27 16:47:49 +0200

    terminate App.py

--- src/emg_bids_converter/_front_end/App.py
+++ src/emg_bids_converter/_front_end/App.py
@@ -1,8 +1,9 @@
 from tkinter import Tk
+from emg_bids_converter._front_end.Home import Home
 
 class App(Tk):
+    """Instantiates the application"""
     def __init__(self):
         super().__init__()
-
         self.withdraw()
-        self.mainloop()
+        Home(self)
